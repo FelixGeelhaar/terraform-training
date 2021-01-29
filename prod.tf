3d7d4e626310c8423b284f238e91d02a0a9fbb60
FelixGeelhaar <felix@felixgeelhaar.de>
2021-01-29 23:54:33 +0100

    load balancer for prod_web

--- prod.tf
+++ prod.tf
@@ -8,7 +8,20 @@ resource "aws_s3_bucket" "prod_tf_course" {
     acl = "private"
 }
 
-resource "aws_default_vpc" "default" {}
+resource "aws_default_vpc" "default" { }
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "eu-central-1a"
+  tags = {
+    "Terraform" = "true"
+  }
+}
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "eu-central-1b"
+  tags = {
+    "Terraform" = "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name = "prod_web"
@@ -68,11 +81,29 @@ resource "aws_instance" "prod_web" {
 }
 
 resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web[0].id
+  instance_id = aws_instance.prod_web.0.id
   allocation_id = aws_eip.prod_web.id
 }
 
 resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name             = "prod-web"
+  instances        = aws_instance.prod_web.*.id
+  subnets          = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
+  security_groups  = [ aws_security_group.prod_web.id ]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
   tags = {
     "Terraform" = "true"
   }
